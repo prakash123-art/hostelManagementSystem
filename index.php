47be27475a567faf61c2ebcf40f8caf7ec3b93a5
prakash123-art <prakashsaud58@gmail.com>
2024-04-26 16:54:06 +0545

    first

---
+++ index.php
@@ -0,0 +1,435 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hostel Allocation System</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="stylecss/styleSheet.css">
+
+    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="animationcss/animate.css">
+    <link rel="stylesheet" type="text/css" href="animationcss/animate.min.css">
+
+    <link rel="stylesheet" href="wow/css/libs/animate.css">
+
+    <link rel="icon" href="icon/hostelicon.ico">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+  </head>
+
+ 
+  <body>
+ 
+   
+
+
+<!----Login In Sign Up Layer-->
+  <header class="loginSign">
+ <nav class="navbar "> 
+       <div class="container">
+         <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#naviColla">
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+
+         <div class="navbar-header">
+          <a href="" class="navbar-brand">HOSTEL ALLOCATION SYSTEM</a>
+         </div>
+         
+         <div class="collapse navbar-collapse" id="naviColla">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#aboutSchool">About School</a></li>
+            <li><a href="#roomsType">Rooms</a></li>
+            <li><a href="#studSatisfi">Satisfication</a></li>
+            <li><a href="../hostel-allocation-system/pdf/help.pdf">Help</a></li>
+            <li><a href="../hostel-allocation-system/admin/index.php">Admin</a></li>
+          </ul>
+          </div>
+       </div>
+     </nav>
+
+<div class="container hero">
+     <div class="row">
+      <div class="col md-12" id="first">
+
+          <h1 >HELLO<br> WELCOME TO SPA COLLEGE HOSTEL</h1>
+          <div class="buttons">
+          <a class="bt bt-signup" href="../hostel-allocation-system/receiptCodeEnter.php">Sign Me Up</a>
+          <a class="bt bt-signin" href="../hostel-allocation-system/student/index.php">Signin</a>
+          </div>
+      </div>
+     </div>
+    </div>
+  </header>
+   
+   <!----About SPA Section---->
+   <section class="features slide slideanim" id="aboutSchool">
+     <h3 class="text-center">ABOUT ME</h3>
+
+     <div class="aboutSchoolParas" >
+     <p class="copy wow bounceInLeft">
+     Sudur Pashchimanchal Academy, established with a nobel aim of imparting education par excellence, is one of the most sucessful leading college in the province.  Since its establishment, the college has gained public attention with meritorious results and exemplary social initiatives.
+
+It is the result of marvelously acclaimed achivements of this institution that only in one and half decade of its conception; its has sucessfully established new parameters of opportunities in education and social activities in the province. Recently the institution has sucessfully established bilateral relations for students exchange with KNU (Kangwon National University), Korea and recently an SPA graduate has been awarded with an honorific sholarship to further her studies in korea.
+
+Education has became something more than possessing a couple of certificate with a university or a board seal on. Realizing this fact team SPA keeps seeking newer Horizons of learning oppurtunities with convenient location, dedicated faculties and staff. With a hope to bring a brighter tomorrow, SPA is always ready to join hands with the prospective young generation in order to achieve life changing  academic goals through collective endeavours of the teachers, students and the college family.</p>
+
+      <p class="copy wow bounceInRight">As students are the future caption of the ship on which we are all sailing through, they are not only the future of their respective parents but also a hope of teachers and our country as well. Considering this fact, I as the chairman of this institution, believe that we have a huge responsibilities upon our shoulders to give our students a favorable ground upon which they develop themselves into a qualified skilled and most importantly a responsible citizen.</p>
+
+      <p class="copy">Our Missions are to produce experts who have the practical ability to invent, install, experiment with, and apply computer hardware and software with a view to making Myanmar a modern and developed nation and to train Information Technology (IT) professionals for Myanmar. 
+     </p>
+     </div>
+
+     <div class="container">
+       <div class="row">
+
+
+         <div class="col-md-3">
+         <i class="fa fa-camera"></i>
+          <h4>Compound</h4>
+           <p class="arranging">
+             University of BIM Studies is located about <b>12 miles</b> from Chauraha.  The compound of SPA  is very wide.  It is amost 70 square kilometer.  There are many buildings, teachers, professors can stay.  It has natural environment. Many trees and woods in the school compound.
+           </p>
+         </div>
+
+         <div class="col-md-3">
+         <i class="fa fa-bicycle"></i>
+         <h4>Campus Activities</h4>
+           <p class="arranging">
+             SPA provied many activities for students to perticipate.  <b>ACM-ICPC</b> International Collegiate Programming Contest, <b>Samsung</b> Tech Institute Android Movile Apps Development Traiining Programs, Parallel Computing, <b>J-Sat</b> Japanese Language course, Moonsoon Season Growing Plants, UCSM's family <b>'Ka-Htain-Pwe'</b> and etc are held in UCSM. Student's Union Orgainzatioin also offers Photo Club, Tech Talk seminor program, Music Club, Dance Club, Chess Club, Art and Literature Club and so on. 
+           </p>
+         </div>
+
+         <div class="col-md-3">
+         <i class="fa fa-certificate"></i>
+         <h4>Fresher Welcome</h4>
+           <p class="arranging">
+            Fresher welcome is always held at the beginning of the academic year.  It's main purpose is to welome new students who choose SPA for their institute.  The rector always give a speech to students. After that SPA offers the prizes to outstanding students that got 1st, 2nd, 3rd rank from final exam.  And then, we choose the campus <b>ANG and QUEEN</b>, fresher <b>KING and QUEEN</b>
+           </p>
+         </div>
+
+         <div class="col-md-3">
+         <i class="fa fa-envelope"></i>
+         <h4>Student Life</h4>
+           <p class="arranging">
+             Student  in SPA have fully compus life because the campus is very large.  Teachers and students are familier.  <b>Clinic</b> and canteens also offer within the campus. Car parking are also large.  Students can also access full time <b>Wi-Fi</b> internet services.    
+           </p>
+         </div>
+
+
+       </div>
+
+
+     </div>
+
+   </section>
+
+   <section class="meal wow slideOutLeft">
+     <ul class="meal-showcase clearfix">
+       <li>
+         <figure class="meal-photo">
+           <img src="images/a.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/b.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/c.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/d.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/e.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/f.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/g.jpg">
+         </figure>
+       </li>
+
+       <li>
+         <figure class="meal-photo">
+           <img src="images/h.jpg">
+         </figure>
+       </li>
+     </ul>
+   </section>
+
+   <section class="mobile slide slideanim">
+     <h3 class="text-center">GET EASIER WITH MOBILE VIEW</h3>
+     <div class="container">
+       
+       <div class="row">
+        <div class="col-md-6">
+          <div class="iPhonePic">
+          <img src="images/iphone.png">
+          </div>
+        </div>
+
+        <div class="work-steps">
+          <div>1</div>
+          <p>Compitable with all screen sizes.</p>
+
+          <div>2</div>
+          <p>User friendly.  Easy to use.</p>
+
+          <div>3</div>
+          <p>Support all browser. </p>
+          
+          <br>
+
+          <a href=""><img src="images/safari.png"></a>
+          <a href=""><img src="images/chrome.png"></a>
+          <a href=""><img src="images/firefox.png"></a>
+         
+
+        </div> 
+
+       </div>
+
+     </div>
+   </section>
+
+   <section class="cities slide slideanim" id="roomsType">
+     <h3 class="text-center">HOSTEL ROOM TYPES</h3>
+     <div class="container">
+       <div class="row">
+         <div class="col-md-3">
+           <img src="images/room1.jpg">
+           <h4>Male Room Type(1)</h4>
+           <p><i class="fa fa-cog"></i>Single room for one person.</p>
+         </div>
+
+          <div class="col-md-3">
+           <img src="images/room2.jpg">
+           <h4>Male Room Type(2)</h4>
+           <p><i class="fa fa-cog"></i>Room for 4+ students.</p>
+         </div>
+
+          <div class="col-md-3">
+           <img src="images/room3.jpg">
+           <h4>Female Room Type(1)</h4>
+           <p><i class="fa fa-cog"></i>3 students can be live in this room.</p>
+         </div>
+
+          <div class="col-md-3">
+           <img src="images/room4.jpg">
+           <h4>Female Room Type(2)</h4>
+           <p><i class="fa fa-cog"></i>Room for 2 students.</p>
+         </div>
+
+
+       </div>
+     </div>
+
+
+
+
+   </section>
+
+   <section class="students slide slideanim" id="studSatisfi">
+     <h3 class="text-center">STUDENTS SATISFACTION</h3>
+
+     <div class="container">
+       
+       <div class="row">
+         <div class="col-md-4">
+           <p>
+             <q><b>SPA</b> The importance of the social environment in students' life cannot be
+overemphasized. One of the key features students and their parents are
+concerned about when enrolling in a university is the availability of student
+housing. The significance of housing as a major determinant of man's
+welfare, life sustenance and survival cannot be over emphasized <b>FEES</b> </q>
+           </p>
+
+           <cite><img src="images/student1.jpg">Sai </cite>
+         </div>
+
+          <div class="col-md-4">
+           <p>
+             <q><b>SPA</b> The study has revealed the facilities available in the hostels in The
+SPA college, <b>FEES</b> 
+              </q>
+           </p>
+
+           <cite><img src="images/student2.jpg">Ram </cite>
+         </div>
+
+          <div class="col-md-4">
+           <p>
+             <q><b>SPA</b> The students' housing is said to include facilities such as bedrooms
+which can serve the dual purpose of study and sleeping, bathrooms and
+toilets, kitchen, laundry, recreational areas and access to internet services as
+this further enhance the study-learning experience.on his
+part, stated that the student housing can be further made exciting by
+providing further facilities such as ATM machines, car park, mini markets,
+bookshops and cafeterias within the precinct of the students' housing. <b>FEES</b> </q> 
+           </p>
+
+           <cite><img src="images/student3.jpg">Nyein </cite>
+         </div>
+       </div>
+     </div>
+
+   </section>
+
+   <section class="form slide slideanim" id="cmt">
+     <div class="container">
+        <h3 class="text-center">WE ARE WAITING FOR YOUR COMMENTS</h3>
+
+        <div class="row gap">
+          <div class="col-md-4">
+            
+              <input type="text" placeholder="Username" class="form-control"><br>
+              <input type="email" placeholder="Email" class="form-control"><br>
+              <input type="password" placeholder="Password" class="form-control">
+            
+
+          </div>
+
+          <div class="col-md-8">
+            <textarea class="form-control" rows="8" placeholder="Comments"></textarea>
+            <button class="bt bt-submit">Submit</button>
+          </div>
+        </div>
+     </div>
+   </section>
+
+   <section class="google">
+    <div class="container-fluid">
+    <h3 class="text-center">S.P.A. College</h3>
+    <div id="googleMap" style="height:400px; width:100%;">
+     <!--Adding Google Maps-->
+     <script src="http://maps.googleapis.com/maps/api/js"></script>
+     <script>
+       var myCenter = new google.maps.LatLng(22.122239,96.185254);
+
+       function initialize(){
+        var mapProp = {
+          center:myCenter,
+          zoom:16,
+          scrollwheel:false,
+          draggable:false,
+          mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        var marker =new google.maps.Marker({
+          position:myCenter,
+        });
+        marker.setMap(map);
+
+       }
+
+       google.maps.event.addDomListener(window,'load', initialize);
+     </script>
+
+</div>
+     </div>
+   </section>
+
+   <!--End Contact-->
+   <footer>
+     <div class="footer slide slideanim" id="footer">
+       <div class="container">
+         <div class="row">
+           <div class="col-md-4 col-lg-4">
+            <h4>Contact Us</h4>
+             <p><i class="fa fa-university" aria-hidden="true"></i>Sudur Pashchimanchal Academy (SPA), Santoshi tole -2 Dhangadhi, Nepal</p>
+             <p><i class="fa fa-globe" aria-hidden="true"></i>spadhangadhi@gmail.com</p>
+           </div>
+
+            <div class="col-md-4 col-lg-4">
+             <h4>Developed By</h4>
+             <p><i class="fa fa-user-circle-o" aria-hidden="true"></i>Prakash</p>
+
+             
+           </div>
+
+            <div class="col-md-4 col-lg-4">
+             <h4>Stay In Touch</h4>
+             <a href="https://www.facebook.com"><i class="fa fa-facebook social" aria-hidden="true"></i></a>
+             <a href="https://www.twitter.com"><i class="fa fa-twitter social" aria-hidden="true"></i></a>
+             <a href="https://www.linkedin.com"><i class="fa fa-linkedin social" aria-hidden="true"></i></a>
+             <a href="https://www.youtube.com"><i class="fa fa-youtube social" aria-hidden="true"></i></a>
+             <a href="https://www.pinterest.com"><i class="fa fa-pinterest social" aria-hidden="true"></i></a>
+             <a href="https://www.github.com"><i class="fa fa-github social" aria-hidden="true"></i><br></a>
+
+
+           </div>
+
+         </div>
+       </div>
+
+     </div>
+
+   </footer>
+
+     <script src="js/wow.min.js"></script>
+      <script>
+              new WOW().init();
+     </script>
+
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+
+        <script >
+          $(window).scroll(function(){
+            $(".slideanim").each(function()){
+            var pos=$(this).offset().top;
+
+            var winTop =   $(window).scrollTop();
+            if(pos< winTop+600){
+              $(this).addClass("slide");
+            }
+          });
+          });
+           </script>
+
+  </body>
+
+</html>
